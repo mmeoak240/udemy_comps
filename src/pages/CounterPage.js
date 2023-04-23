a09a71c451ca983cfe063b7e226a428671883421
Michael Meoak <mmeoak240@gmail.com>
2023-04-23 15:49:30 -0700

    added immer

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,3 +1,4 @@
+import { produce } from "immer";
 import Button from "../components/Button";
 import { useReducer } from "react";
 import Panel from "../components/Panel";
@@ -11,31 +12,23 @@ const SUBMIT_ADD_FORM = "submit-add-form";
 const reducer = (state, action) => {
 	switch (action.type) {
 		case INCREMENT_COUNT:
-			return {
-				...state,
-				count: state.count + 1,
-			};
+			state.count = state.count + 1;
+			return;
 
 		case DECREMENT_COUNT:
-			return {
-				...state,
-				count: state.count - 1,
-			};
+			state.count = state.count - 1;
+			return;
 
 		case SET_VALUE_TO_ADD:
-			return {
-				...state,
-				valueToAdd: action.payload,
-			};
+			state.valueToAdd = action.payload;
+			return;
 
 		case SUBMIT_ADD_FORM:
-			return {
-				...state,
-				count: state.count + state.valueToAdd,
-				valueToAdd: 0,
-			};
+			state.count = state.count + state.valueToAdd;
+			state.valueToAdd = 0;
+			return;
 		default:
-			throw new Error("unexpected action type:" + action.type);
+			return;
 	}
 };
 
@@ -43,7 +36,7 @@ function CounterPage({ initialCount }) {
 	// const { count, increment } = useCounter(initialCount);
 	// const [count, setCount] = useState(initialCount);
 	// const [valueToAdd, setValueToAdd] = useState(0);
-	const [state, dispatch] = useReducer(reducer, {
+	const [state, dispatch] = useReducer(produce(reducer), {
 		count: initialCount,
 		valueToAdd: 0,
 	});
